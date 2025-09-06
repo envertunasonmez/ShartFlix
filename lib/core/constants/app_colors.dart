006ab99c5e1b43e2fa05dc05b2abe972946a22c1
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-07 01:12:24 +0300

    create - app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Brand Colors
+  static const Color primary = Color(0xFFE50914);
+  static const Color primaryDark = Color(0xFF6F060B);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+
+  // White Opacity Variants
+  static Color white5 = whiteColor.withOpacity(0.05);
+  static Color white10 = whiteColor.withOpacity(0.10);
+  static Color white20 = whiteColor.withOpacity(0.20);
+  static Color white30 = whiteColor.withOpacity(0.30);
+  static Color white40 = whiteColor.withOpacity(0.40);
+  static Color white50 = whiteColor.withOpacity(0.50);
+  static Color white60 = whiteColor.withOpacity(0.60);
+  static Color white70 = whiteColor.withOpacity(0.70);
+  static Color white80 = whiteColor.withOpacity(0.80);
+  static Color white90 = whiteColor.withOpacity(0.90);
+
+  // Functional Colors
+  static const Color secondary = Color(0xFF5949E6);
+  static const Color success = Color(0xFF00C247);
+  static const Color info = Color(0xFF004CE8);
+  static const Color warning = Color(0xFFFFBE16);
+  static const Color error = Color(0xFFF47171);
+}
