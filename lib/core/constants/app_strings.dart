10c5f50cf5c6c146447d5c620fa99617d7439d62
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-07 01:12:32 +0300

    create- app strings

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,17 @@
+class AppStrings {
+  //String Constants
+  static const String appName = 'Shartflix';
+
+  // Assets Constants
+  static const String logoPath = 'assets/images/shartflix_logo.svg';
+  static const String splashAnimationPath = 'assets/splash_animation.json';
+  static const String googleIconPath = 'assets/icons/ic_google.svg';
+  static const String appleIconPath = 'assets/icons/ic_apple.svg';
+  static const String facebookIconPath = 'assets/icons/ic_facebook.svg';
+
+  // Icons Constants
+  static const String emailIconPath = 'assets/icons/ic_mail.svg';
+  static const String passwordIconPath = 'assets/icons/ic_password.svg';
+  static const String hideIconPath = 'assets/icons/ic_hide.svg';
+  static const String userIconPath = 'assets/icons/ic_user.svg';
+}
