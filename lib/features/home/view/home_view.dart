83e848bf3d2b014db4cdff0c57c00db2b3c0791a
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-07 19:28:56 +0300

    update - home view

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:jr_case_boilerplate/core/constants/app_colors.dart';
+import 'package:jr_case_boilerplate/core/constants/app_strings.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            width: size.width,
+            height: size.height,
+            child: SvgPicture.asset(AppStrings.homeImage, fit: BoxFit.cover),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 280,
+              width: size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  stops: const [0.0, 0.33, 1.0],
+                  colors: [
+                    AppColors.blackColor,
+                    AppColors.blackColor,
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
