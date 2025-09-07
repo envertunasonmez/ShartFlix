a0f23c8ebba53214e6f83f30f067d2de63e4ecdf
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-07 17:07:35 +0300

    add -  custom background

--- lib/features/splash/view/splash_view.dart
+++ lib/features/splash/view/splash_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jr_case_boilerplate/core/constants/app_colors.dart';
 import 'package:jr_case_boilerplate/core/constants/app_strings.dart';
 import 'package:jr_case_boilerplate/core/constants/app_text_styles.dart';
+import 'package:jr_case_boilerplate/core/widgets/background/custom_background.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -10,19 +11,21 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SvgPicture.asset(AppStrings.logoPath, height: 120),
-            Text(
-              AppStrings.appName,
-              style: AppTextStyles.heading40.copyWith(
-                color: AppColors.whiteColor,
+      body: CustomBackground(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(AppStrings.logoPath, height: 120),
+              const SizedBox(height: 16),
+              Text(
+                AppStrings.appName,
+                style: AppTextStyles.heading40.copyWith(
+                  color: AppColors.whiteColor,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
