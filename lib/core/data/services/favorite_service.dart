de148bda2845b78988e820564f8bc9f3ceca6793
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-10 15:29:59 +0300

    add - get favorites

--- lib/core/data/services/favorite_service.dart
+++ lib/core/data/services/favorite_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:jr_case_boilerplate/core/data/models/favorite_movie_list_response_model.dart';
 import '../models/favorite_request_model.dart';
 import '../models/favorite_response_model.dart';
 import '../storage/token_storage.dart';
@@ -13,11 +14,20 @@ class FavoriteService {
 
     final response = await _dio.post(
       "movie/favorite/${model.favoriteId}",
-      options: Options(
-        headers: {"Authorization": "Bearer $token"},
-      ),
+      options: Options(headers: {"Authorization": "Bearer $token"}),
     );
 
     return FavoriteResponseModel.fromJson(response.data);
+  }
+
+  Future<FavoriteMovieListResponseModel> getFavorites() async {
+    final token = await TokenStorage.getToken();
+
+    final response = await _dio.get(
+      "movie/favorites",
+      options: Options(headers: {"Authorization": "Bearer $token"}),
+    );
+
+    return FavoriteMovieListResponseModel.fromJson(response.data);
   }
 }
