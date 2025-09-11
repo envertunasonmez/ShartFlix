00aa61cb586fa80ebd9f604042e73e3a033d7925
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-11 15:03:01 +0300

    add - image paths

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -7,6 +7,10 @@ class AppStrings {
   static const String splashAnimationPath = 'assets/splash_animation.json';
   static const String homeImage = 'assets/images/home_image.png';
   static const String circleLogo = 'assets/images/circle_logo.svg';
+  static const String premiumAcc = 'assets/images/premium_acc.png';
+  static const String moreMatches = 'assets/images/more_matches.png';
+  static const String highlight = 'assets/images/highlight.png';
+  static const String moreLikes = 'assets/images/more_likes.png';
 
   // Icons Constants
   static const String emailIconPath = 'assets/icons/ic_mail.svg';
