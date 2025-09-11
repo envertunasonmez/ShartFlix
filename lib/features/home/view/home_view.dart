9118b1005329d4be3658cadb7e28af7a9f0c93a5
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-11 17:30:38 +0300

    remove - unnecessary scroll when we are on top

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -10,36 +10,51 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<MovieBloc, MovieState>(
-        builder: (context, state) {
-          if (state is MovieLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is MovieLoaded) {
-            final movies = state.response.movies;
+      body: ScrollConfiguration(
+        behavior: const _NoGlowScrollBehavior(),
+        child: BlocBuilder<MovieBloc, MovieState>(
+          builder: (context, state) {
+            if (state is MovieLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is MovieLoaded) {
+              final movies = state.response.movies;
 
-            return PageView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: movies.length,
-              itemBuilder: (context, index) {
-                final movie = movies[index];
-                return HomeMovieListItem(
-                  title: movie.title,
-                  description: movie.description,
-                  image: movie.posterUrl,
-                  movieId: movie.id, 
-                  onFavTap: (isFav) {
-                    print("Fav durumu değişti: $isFav, film id: ${movie.id}");
-                  },
-                );
-              },
-            );
-          } else if (state is MovieError) {
-            return Center(child: Text("Hata: ${state.message}"));
-          } else {
-            return const Center(child: Text("Film bulunamadı"));
-          }
-        },
+              return PageView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: movies.length,
+                itemBuilder: (context, index) {
+                  final movie = movies[index];
+                  return HomeMovieListItem(
+                    title: movie.title,
+                    description: movie.description,
+                    image: movie.posterUrl,
+                    movieId: movie.id,
+                    onFavTap: (isFav) {
+                      print("Fav durumu değişti: $isFav, film id: ${movie.id}");
+                    },
+                  );
+                },
+              );
+            } else if (state is MovieError) {
+              return Center(child: Text("Hata: ${state.message}"));
+            } else {
+              return const Center(child: Text("Film bulunamadı"));
+            }
+          },
+        ),
       ),
     );
+  }
+}
+
+class _NoGlowScrollBehavior extends ScrollBehavior {
+  const _NoGlowScrollBehavior();
+  @override
+  Widget buildOverscrollIndicator(
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
+    return child;
   }
 }
