b82a7586a7967512815818658c34fee5376f53b8
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-09-12 20:34:47 +0300

    fix - login view

--- lib/features/auth/views/login/login_view.dart
+++ lib/features/auth/views/login/login_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jr_case_boilerplate/core/constants/app_colors.dart';
 import 'package:jr_case_boilerplate/core/widgets/background/custom_background.dart';
+import 'package:jr_case_boilerplate/cubit/validation/validation_cubit.dart';
 import 'package:jr_case_boilerplate/features/auth/views/login/widgets/login_header.dart';
 import 'package:jr_case_boilerplate/features/auth/views/login/widgets/login_form.dart';
 import 'package:jr_case_boilerplate/features/auth/views/login/widgets/register_redirect.dart';
@@ -16,6 +18,14 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,27 +37,31 @@ class _LoginViewState extends State<LoginView> {
       body: CustomBackground(
         gradientStart: AppColors.primary,
         gradientMiddle: AppColors.primaryDark.withOpacity(0.5),
-        child: SingleChildScrollView(
-          physics: const ClampingScrollPhysics(),
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: width * 0.05,
-              vertical: height * 0.02,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                LoginHeader(height: height, width: width),
-                LoginForm(
-                  emailController: _emailController,
-                  passwordController: _passwordController,
-                  width: width,
-                ),
-                const SizedBox(height: 20),
-                SocialButtons(width: width),
-                const SizedBox(height: 16),
-                RegisterRedirect(width: width),
-              ],
+        child: BlocProvider(
+          create: (context) => FormValidationCubit(),
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: width * 0.05,
+                vertical: height * 0.02,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  LoginHeader(height: height, width: width),
+                  LoginForm(
+                    emailController: _emailController,
+                    passwordController: _passwordController,
+                    width: width,
+                    formKey: _formKey,
+                  ),
+                  const SizedBox(height: 20),
+                  SocialButtons(width: width),
+                  const SizedBox(height: 16),
+                  RegisterRedirect(width: width),
+                ],
+              ),
             ),
           ),
         ),
